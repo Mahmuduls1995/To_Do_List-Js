be8cb30e819dcc04129a17ed3a259e179953ca14
mahmudul1995 <mahmuduls451@gmail.com>
2022-03-11 23:10:16 +0600

    second commit

--- project.js
+++ project.js
@@ -8,10 +8,28 @@ document.getElementById("add-btn").addEventListener("click", function () {
     alert("Please enter a value");
   } 
   
+  else {
+    const mainContainer = document.getElementById("content-container");
+    const tableContainer = document.createElement("tr");
+    tableContainer.innerHTML = `<th>${count}</th>
+      <td>${inputValue}</td>
+      <td><button class="btn btn-danger delete-btn">Delete</button>
+      <button class="btn btn-success done-btn">Done</button></td>`;
+    mainContainer.appendChild(tableContainer);
+
+    document.getElementById("input-value").value = "";
+
+    const deleteButton = document.getElementsByClassName("delete-btn");
+
+    const doneButton = document.getElementsByClassName("done-btn");
+
+    
+
+
+  }
+
+
+
+
   
-
-
-
-
-
 });
