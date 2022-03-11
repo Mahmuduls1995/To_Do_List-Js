b60271ff4e50f4bdb3a37a057394bf4027922ce9
mahmudul1995 <mahmuduls451@gmail.com>
2022-03-11 23:07:44 +0600

    first commit

---
+++ project.js
@@ -0,0 +1,17 @@
+let count = 0;
+
+document.getElementById("add-btn").addEventListener("click", function () {
+  count++;
+  const inputValue = document.getElementById("input-value").value;
+
+  if (inputValue == "") {
+    alert("Please enter a value");
+  } 
+  
+  
+
+
+
+
+
+});
